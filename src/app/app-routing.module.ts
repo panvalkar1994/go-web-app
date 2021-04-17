b92de4e2ee165bcb640a52f930ff409f1330ed72
vaibhav <vpanvalkar@outlook.com>
2021-04-17 16:59:26 +0530

    main routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { OffersComponent } from './offers/offers.component';
+import { AppSettingsComponent } from './app-settings/app-settings.component';
+import { AppSearchComponent } from './app-search/app-search.component';
+import { BookingDetailsComponent } from './booking-details/booking-details.component';
+import { CancellationsComponent } from './cancellations/cancellations.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'settings', component: AppSearchComponent },
+  { path: 'search', component: AppSearchComponent },
+  { path: 'bookings', component: BookingDetailsComponent },
+  { path: 'cancellations', component: CancellationsComponent },
+  { path: 'us', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  ProfileComponent,
+  OffersComponent,
+  AppSettingsComponent,
+  AppSearchComponent,
+  BookingDetailsComponent,
+  CancellationsComponent,
+  AboutUsComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { MaterialModule } from './material/material.module';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    routingComponents,
+    
   ],
   imports: [
     BrowserModule,
