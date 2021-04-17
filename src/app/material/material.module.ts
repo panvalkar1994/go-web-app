cf4324e020eb8750a88b6120e914ee1b8f0f8e93
vaibhav <vpanvalkar@outlook.com>
2021-04-17 13:14:02 +0530

    nav bar added to app and material module added

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+const material = [
+  LayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+]
+
+@NgModule({
+  imports: [material],
+  exports:[material]
+})
+export class MaterialModule { }
